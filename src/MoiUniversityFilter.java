5b2bacc8806673cb5f21b26a9b08099fe9c8f628
Lawrence-ekale <lawrenceekale2@gmail.com>
2023-05-31 09:47:05 +0300

    Late comers and early exit report

--- src/MoiUniversityFilter.java
+++ src/MoiUniversityFilter.java
@@ -372,6 +372,256 @@ public class MoiUniversityFilter {
 		}
 		
 	}
+	public void getReportClock(String school) {
+		HashMap<Employee,Integer> myAbsentees = new HashMap<>();
+		int monthDays = 30;
+        DateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date;
+		DataFormatter dataFormatter= new DataFormatter();
+		try {
+		    File file = new File("/home/lawrence/Music/MoiUniversity/Nairobi Campus April 2023 (1)(1).xls");
+		    Workbook wb;
+
+		    if (file.getName().endsWith(".xlsx")) {
+		        // For .xlsx files
+		        wb = new XSSFWorkbook(file);
+		        
+		    } else {
+		        // For .xls files
+		        wb = new HSSFWorkbook(new FileInputStream(file));
+		    }
+
+		    // Get the first sheet of the workbook
+		    Sheet sheet = wb.getSheetAt(0);
+
+		    //FormulaEvaluator formulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
+			
+			int firstRow = 0;//escape the first row
+			for(Row row: sheet) {
+				
+				if(firstRow != 0) {
+					String name = "";
+					String pfNo = "";
+					//Boolean present = true;
+					//int lastColumn = Math.max(row.getLastCellNum(), 0);
+						Cell cellIn = row.getCell(6, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+						String cellInValue = dataFormatter.formatCellValue(cellIn);
+
+						//start
+						if(!cellInValue.isEmpty()) { //if clocked in
+							String [] values = cellInValue.split(":");
+							
+							if(Integer.parseInt(values[0]) < 9) { //if stayed less than 9 hours
+								
+								Cell cell5 = row.getCell(0, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+				            	pfNo = dataFormatter.formatCellValue(cell5);
+				            	cell5 = row.getCell(1, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+				            	name = dataFormatter.formatCellValue(cell5);
+				            	Employee emp = new Employee(name,pfNo);
+				            	
+				                if (myAbsentees.containsKey(emp)) {
+				                	//System.out.print(emp.getName() + "\t\t" + emp.getPfNo() + "\t\t " + myAbsentees.get(emp)+"\n");
+				                	myAbsentees.put(emp, myAbsentees.get(emp) + 1);
+				                } else {
+				                	myAbsentees.put(emp, 1);
+				                }
+								
+							}
+							
+						}
+				
+			}
+				firstRow = 1;
+		}
+			
+			//Sort the hashmap according to absentees that is ascending order
+			myAbsentees = sortByValueAbsentees(myAbsentees);
+			//Done processing
+			// Create a PDF document
+			// Create a PDF document
+
+
+			PDDocument document = new PDDocument();
+			PDPage page = new PDPage();
+			document.addPage(page);
+
+			// Set font and font size
+			PDPageContentStream contentStream = new PDPageContentStream(document, page);
+			contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+
+			// Define table parameters
+			float margin = 50;
+			float yStart = page.getMediaBox().getHeight() - margin - 100;
+			float yPosition = yStart;
+			float tableHeight = 20;
+			
+			File logoFile = new File("resources/moi.jpeg");
+			PDImageXObject logoImage = PDImageXObject.createFromFileByExtension(logoFile, document);
+			float logoX = 250; // Set the X-coordinate of the logo
+			float logoY = yStart; // Set the Y-coordinate of the logo
+			float logoWidth = 100; // Set the width of the logo
+			float logoHeight = 120; // Set the height of the logo
+			
+			contentStream.drawImage(logoImage, logoX, logoY, logoWidth, logoHeight);
+			PDFont boldFont = PDType1Font.TIMES_BOLD;
+			float fontSize = 12;
+			
+			contentStream.beginText();
+			contentStream.setFont(boldFont, fontSize);
+			float textX = logoX + logoWidth + 10; // Set the X-coordinate of the text
+			float textY = logoY + logoHeight / 2 - fontSize / 2; // Set the Y-coordinate of the text
+			contentStream.newLineAtOffset(textX-110, textY - 60);
+			contentStream.showText("MOI UNIVERSITY");
+			contentStream.setFont(boldFont, 11);
+			float secondTextX = textX - 55; // Set the X-coordinate of the second text same as the first text
+			float secondTextY = textY - 10; // Set the Y-coordinate of the second text below the first text
+			contentStream.moveTextPositionByAmount(secondTextX - textX, secondTextY - textY);
+			contentStream.showText("OFFICE OF THE DEPUTY VICE-CHANCELLOR");
+			
+			float thirdTextX = textX - 60; // Set the X-coordinate of the third text same as the second text
+			float thirdTextY = secondTextY - 10; // Set the Y-coordinate of the third text below the second text
+			contentStream.moveTextPositionByAmount(thirdTextX - secondTextX, thirdTextY - secondTextY);
+			contentStream.showText("ADMINISTRATION, PLANNING AND STRATEGY");
+			contentStream.endText();
+			
+			contentStream.setFont(boldFont, fontSize);
+			yStart -= 45;
+			// Draw table header
+			contentStream.setLineWidth(1.0f);
+			contentStream.beginText();
+			contentStream.newLineAtOffset(margin, yStart);
+			contentStream.showText("PF-No.");
+			contentStream.newLineAtOffset(50, 0);
+			contentStream.showText("Name");
+			contentStream.newLineAtOffset(130, 0);
+			contentStream.showText("No. Of Days (Less than 9 hours)");
+			contentStream.endText();
+
+			// Print the data to the PDF
+			yStart -= 45;
+
+			//PDPageContentStream contentStream = new PDPageContentStream(document, page);  // Use the contentStream for the current page
+			List<PDPage> pages = new ArrayList<>();
+			PDPageContentStream newContentStream = null;
+			int counter = 1;
+			for (Map.Entry<Employee, Integer> entry : myAbsentees.entrySet()) {
+			    if (counter % 29 == 0) {
+			        // Create a new page and add it to the document
+			        PDPage newPage = new PDPage();
+			        pages.add(newPage);
+			        document.addPage(newPage);
+
+			        // Start a new content stream on the new page
+			        if (newContentStream != null) {
+			            newContentStream.close();
+			        }
+			        newContentStream = new PDPageContentStream(document, newPage);
+			        yPosition = newPage.getMediaBox().getHeight() - margin - 55; 
+			    }
+			    
+			    if (newContentStream != null) {
+			        newContentStream.beginText();
+			        newContentStream.setFont(PDType1Font.TIMES_ROMAN, 10);
+			        newContentStream.newLineAtOffset(margin, yPosition);
+			        newContentStream.showText(entry.getKey().getPfNo());
+			        newContentStream.newLineAtOffset(50, 0);
+			        newContentStream.showText(entry.getKey().getName());
+			        newContentStream.newLineAtOffset(170, 0);
+			        newContentStream.showText(entry.getValue()+"");
+			        newContentStream.endText();
+
+			        yPosition -= tableHeight;
+			    } else {
+			        // Continue writing to the current page's content stream
+			        contentStream.beginText();
+			        contentStream.setFont(PDType1Font.HELVETICA, 10);
+			        contentStream.newLineAtOffset(margin, yPosition - 55);
+			        contentStream.showText(entry.getKey().getPfNo());
+			        contentStream.newLineAtOffset(50, 0);
+			        contentStream.showText(entry.getKey().getName());
+			        contentStream.newLineAtOffset(170, 0);
+			        contentStream.showText(entry.getValue()+"");
+			        contentStream.endText();
+			        yPosition -= tableHeight;
+			    }
+			    
+			    counter++;
+			}
+			
+			// Add the signature on the last page
+			if (!pages.isEmpty()) {
+			    PDPage lastPage = pages.get(pages.size() - 1);
+			    PDPageContentStream lastPageContentStream = new PDPageContentStream(document, lastPage, PDPageContentStream.AppendMode.APPEND, true, true);
+			    lastPageContentStream.beginText();
+			    lastPageContentStream.setFont(PDType1Font.TIMES_BOLD, 12);
+			    lastPageContentStream.newLineAtOffset(margin, yPosition - 55);
+			    lastPageContentStream.showText("PROF. ENG. KIRIMI H. KIRIAMITI");
+			    lastPageContentStream.newLineAtOffset(0, -10);
+			    lastPageContentStream.showText("DEPUTY  VICE-CHANCELLOR, ADMINISTRATION, PLANNING & STRATEGY");
+			    //lastPageContentStream.newLineAtOffset(300, -5);
+			    lastPageContentStream.newLineAtOffset(0, -40);
+			    lastPageContentStream.showText("Staff attendance register for : "+school+" Campus,  Generated on " + dateFormat.format(new Date())+ " for April");
+			    lastPageContentStream.endText();
+
+				float lineY = yPosition - 67; // Adjust the Y-coordinate of the line beneath the second text
+				
+				float lineWidth = PDType1Font.TIMES_ROMAN.getStringWidth("DEPUTY VICE-CHANCELLOR, ADMINISTRATION, PLANNING & STRATEGY") / 1000 * 10;
+				
+				lastPageContentStream.setLineWidth(0.5f); // Set the line width
+				lastPageContentStream.moveTo(margin, lineY);
+				lastPageContentStream.lineTo(margin + lineWidth + 95, lineY);
+				lastPageContentStream.stroke();
+				
+				
+			    lastPageContentStream.close();
+			}else {
+			    // Handle the case when there are no pages or only one page
+			    /*PDPage newPage = new PDPage();
+			    document.addPage(newPage);
+			    
+			    PDPageContentStream newPageContentStream = new PDPageContentStream(document, newPage);*/
+			    contentStream.beginText();
+			    contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
+			    contentStream.newLineAtOffset(margin, yPosition - 75);
+			    contentStream.showText("PROF. ENG. KIRIMI H. KIRIAMITI");
+			    contentStream.newLineAtOffset(0, -10);
+			    contentStream.showText("DEPUTY VICE-CHANCELLOR, ADMINISTRATION, PLANNING & STRATEGY");
+			    contentStream.newLineAtOffset(0, -40);
+			    contentStream.showText("Staff attendance register for: " + school + " Campus, Generated on " + dateFormat.format(new Date()) + " for April");
+			    contentStream.endText();
+			    
+			    float lineY = yPosition - 90; // Adjust the Y-coordinate of the line beneath the second text
+			    float lineWidth = PDType1Font.TIMES_ROMAN.getStringWidth("DEPUTY VICE-CHANCELLOR, ADMINISTRATION, PLANNING & STRATEGY") / 1000 * 10;
+			    
+			    contentStream.setLineWidth(0.5f); // Set the line width
+			    contentStream.moveTo(margin, lineY);
+			    contentStream.lineTo(margin + lineWidth + 95, lineY);
+			    contentStream.stroke();
+			    
+			    contentStream.close();
+			}
+
+			// Close the content streams
+			if (contentStream != null) {
+			    contentStream.close();
+			}
+			if (newContentStream != null) {
+			    newContentStream.close();
+			}
+
+			// Save and close the document
+			File outputFile = new File("/home/lawrence/Desktop/nairobi.pdf");
+			document.save(outputFile);
+			document.close();
+
+		} catch (IOException e) {
+			
+		    e.printStackTrace();
+		} catch (InvalidFormatException e) {
+		    e.printStackTrace();
+		}
+		
+	}
 	
     public static HashMap<Employee, Integer[]> sortByValue(HashMap<Employee, Integer[]> hm,final int monthDays, final int weekends)
     {
@@ -393,6 +643,26 @@ public class MoiUniversityFilter {
         
         return sortedHashMap;
     }
+    
+    public static HashMap<Employee, Integer> sortByValueAbsentees(HashMap<Employee, Integer> hm) {
+    	
+        List<Map.Entry<Employee, Integer>> entryList = new ArrayList<>(hm.entrySet());
+        Collections.sort(entryList, new Comparator<Map.Entry<Employee, Integer>>() {
+            public int compare(Map.Entry<Employee, Integer> entry1, Map.Entry<Employee, Integer> entry2) {
+                //return Integer.compare(value2, value1);
+                return entry1.getValue().compareTo(entry2.getValue());
+                //(monthDays - (entry.getValue()[0] + weekends) + (entry.getValue()[1]))
+            }
+        });
+
+        // Create a new LinkedHashMap to store the sorted entries
+        LinkedHashMap<Employee, Integer> sortedHashMap = new LinkedHashMap<>();
+        for (Map.Entry<Employee, Integer> entry : entryList) {
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+        
+        return sortedHashMap;
+    }
 
 	public static void main(String args[]) throws IOException {
 		MoiUniversityFilter mu = new MoiUniversityFilter();
@@ -409,7 +679,8 @@ public class MoiUniversityFilter {
             String campus = sc.nextLine();
 
             // Use the input values as needed
-            mu.getReport(weekends,holidays,campus);
+            //mu.getReport(weekends,holidays,campus);
+            mu.getReportClock(campus);
         } catch (InputMismatchException e) {
             System.out.println("Invalid input. Please enter integer values.");
         }
